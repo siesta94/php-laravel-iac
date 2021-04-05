fcd36416539fd9b18006b049cf60b4fc47a626d7
root <root@ip-172-16-51-6.ec2.internal>
2021-04-05 14:25:34 +0000

    Sorting variables

--- modules/compute/lc-asg.tf
+++ modules/compute/lc-asg.tf
@@ -6,7 +6,7 @@ resource "aws_launch_configuration" "ecs_lc" {
   instance_type   = var.ecs_instance_type
   security_groups = [aws_security_group.pm4_web_sg.id]
   key_name        = aws_key_pair.pm4_nat_key.id
-  user_data       = file("userdata")
+  #user_data       = file("userdata")
 
   lifecycle {
     create_before_destroy = true
--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,3 +2,10 @@ variable "vpc_cidr" {}
 variable "cidrs" {
   type = map(string)
 }
+variable "nat_ami" {}
+variable "nat_instance_type" {}
+variable "nat_key" {}
+variable "ecs_instance_ami" {}
+variable "ecs_instance_type" {}
+variable "tasks_instance_ami" {}
+variable "tasks_instance_type" {}
